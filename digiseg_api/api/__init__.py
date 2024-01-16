167cdaf36269b5b1f7c3cfce69c86ada416569ed
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2024-01-16 19:31:18 +0100

    Generated code

---
+++ digiseg_api/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from digiseg_api.api.accounts_api import AccountsApi
+from digiseg_api.api.audiences_api import AudiencesApi
+from digiseg_api.api.auth_api import AuthApi
+from digiseg_api.api.users_api import UsersApi
+
